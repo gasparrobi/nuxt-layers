8fffe36522d69547f6b03727977e31767c1d5f02
gasparrobi <gasparrobi@gmail.com>
2023-12-03 14:11:27 +0100

    extend from gh

--- nuxt-app/nuxt.config.ts
+++ nuxt-app/nuxt.config.ts
@@ -1,7 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   // devtools: { enabled: true },
-  extends: ["../layers/shell", "../layers/utils"],
+  extends: [
+    "github:gasparrobi/nuxt-layers/layers/shell",
+    "github:gasparrobi/nuxt-layers/layers/utils",
+  ],
   typescript: {
     includeWorkspace: true,
   },
